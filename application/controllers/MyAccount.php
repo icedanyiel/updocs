6a97d24dde4910b840618fface87615c70df220d
ArnoldNeuhold <neuhold.arnold95@gmail.com>
2017-05-15 19:43:38 +0300

    Update MyAccount.php
    MyAccount page - if not logged in redirects to login page else

--- application/controllers/MyAccount.php
+++ application/controllers/MyAccount.php
@@ -1,12 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class MyAccount extends CI_Controller {
 
-	public function index()
+function __construct()
+ {
+   parent::__construct();
+ }
+ 
+public function index()
 	{
-        $this->load->view('templates/header');
-        $this->load->view('myaccount');
-        $this->load->view('templates/footer');
-	}
+    if($this->session->userdata('logged_in'))
+	{
+     $session_data = $this->session->userdata('logged_in');
+     $data['email'] = $session_data['email'];
+	 $data['name'] = $session_data['name'];
+     $this->load->view('templates/header',$data);
+     $this->load->view('myaccount',$data);
+     $this->load->view('templates/footer',$data);
+   }
+   else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+  }
+  }
+  
+ function logout()
+ {
+   $this->session->unset_userdata('logged_in');
+   $this->session->sess_destroy();
+   redirect('home', 'refresh');
+ }
+ 
 }
