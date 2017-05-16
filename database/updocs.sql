a000584f078b550a81950aa39049e56dc68977eb
icedanyiel <danyielwest@gmail.com>
2017-05-16 15:55:52 +0300

    new tags table
    new database table: tags
    contains: tag id, tag name and file id which is a foreign key to the id
    from the table 'file'

--- database/updocs.sql
+++ database/updocs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 15 Mai 2017 la 19:20
--- Versiune server: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: 16 Mai 2017 la 14:54
+-- Versiune server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -38,6 +38,18 @@ CREATE TABLE `file` (
 -- --------------------------------------------------------
 
 --
+-- Structura de tabel pentru tabelul `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `file_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structura de tabel pentru tabelul `users`
 --
 
@@ -45,19 +57,15 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `password_conf` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Salvarea datelor din tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `password_conf`) VALUES
-(1, 'admin', 'danyielwest@gmail.com', 'admin', ''),
-(3, 'Andrew', 'email@yahoo.com', '8287458823facb8ff918dbfabcd22ccb', '8287458823facb8ff918dbfabcd22ccb'),
-(4, 'Achileus', 'nume@yahoo.com', '59c5490ada323d9da6263a987f7fcee6', '59c5490ada323d9da6263a987f7fcee6'),
-(6, 'Arnold', 'eu@yahoo.com', '8287458823facb8ff918dbfabcd22ccb', '8287458823facb8ff918dbfabcd22ccb');
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'admin', 'danyielwest@gmail.com', '21232f297a57a5a743894a0e4a801fc3');
 
 --
 -- Indexes for dumped tables
@@ -69,6 +77,13 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `password_conf`) VALUES
 ALTER TABLE `file`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_iduser` (`iduser`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idfile` (`file_id`);
 
 --
 -- Indexes for table `users`
@@ -86,10 +101,15 @@ ALTER TABLE `users`
 ALTER TABLE `file`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Restrictii pentru tabele sterse
 --
@@ -99,6 +119,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `file`
   ADD CONSTRAINT `fk_iduser` FOREIGN KEY (`iduser`) REFERENCES `users` (`id`);
+
+--
+-- Restrictii pentru tabele `tags`
+--
+ALTER TABLE `tags`
+  ADD CONSTRAINT `fk_idfile` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
