b6ee4226032cfea310b080e65ecd547803b1f255
ArnoldNeuhold <neuhold.arnold95@gmail.com>
2017-05-29 23:54:17 +0300

    Update myaccount.php
    Updated account view that shows your uploaded files

--- application/views/myaccount.php
+++ application/views/myaccount.php
@@ -1,3 +1,36 @@
-<h1>MyAccount</h1><br><br><br><br><br><br>
+<h1>MyAccount</h1>
    <h2>Welcome <?php echo $email; ?>!</h2>
+    
+      <div class="table-responsive">  
+           <table class="table table-bordered">  
+                <tr>  
+                     <th>ID</th>  
+                     <th>File Name</th>     
+                </tr> 
+			
+				
+           <?php  
+           if($fetch_data->num_rows() > 0)  
+           {  
+                foreach($fetch_data->result() as $row)  
+                {  
+           ?>  
+                <tr>  
+                     <td><?php echo $row->id; ?></td>  
+                     <td><?php echo "<a href='"."file:///C:/xampp/htdocs/updocs/public/".$row->filename."'>".$row->filename. "</a>"; ?></td> 
+                      
+                </tr>  
+				<?php       
+                }  
+           }  
+           else  
+           {  
+           ?>  
+                <tr>  
+                     <td colspan="2">No Data Found</td>  
+                </tr>  
+           <?php  
+           }  
+           ?>  
+				
    <a href="MyAccount/logout" class="btn btn-default" >LogOut</a>
