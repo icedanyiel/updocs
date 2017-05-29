20b060f4766a5cdf67dd82ad8819c1ba3c4db420
ArnoldNeuhold <neuhold.arnold95@gmail.com>
2017-05-29 23:53:36 +0300

    Update MyAccount.php
    Updated account controller that shows your uploaded files

--- application/controllers/MyAccount.php
+++ application/controllers/MyAccount.php
@@ -5,19 +5,38 @@ class MyAccount extends CI_Controller {
 function __construct()
  {
    parent::__construct();
+   $this->load->library('session');
+    
  }
  
 public function index()
 	{
+		
+	
     if($this->session->userdata('logged_in'))
 	{
+	//trebe instalat plugin pe Chrome de LocalLinks sa deschida fisierele locale https://chrome.google.com/webstore/detail/locallinks/jllpkdkcdjndhggodimiphkghogcpida
+	$this->load->model("myaccount_model"); 
+	$session_data = $this->session->userdata('logged_in');
+	$id = $session_data['id'];
+	$data["fetch_data"] = $this->myaccount_model->fetch_data($id);
+	
      $session_data = $this->session->userdata('logged_in');
      $data['email'] = $session_data['email'];
-	   $data['name'] = $session_data['name'];
+	 $data['name'] = $session_data['name'];
+	 
+	 
      $this->load->view('templates/header',$data);
      $this->load->view('myaccount',$data);
+	 
+	 
+
+	 
      $this->load->view('templates/footer',$data);
    }
+   
+
+   
    else
    {
      //If no session, redirect to login page
@@ -25,6 +44,7 @@ public function index()
   }
   }
   
+
  function logout()
  {
    $this->session->unset_userdata('logged_in');
