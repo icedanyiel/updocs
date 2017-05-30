c2ba9d837cfd72268213331915f05aad0a1f9a1f
ArnoldNeuhold <neuhold.arnold95@gmail.com>
2017-05-30 21:39:40 +0300

    Update myaccount_model.php
    Delete file from database function

--- application/models/myaccount_model.php
+++ application/models/myaccount_model.php
@@ -7,10 +7,17 @@
            //$query = $this->db->get("tbl_user");  
            //select * from tbl_user  
            //$query = $this->db->query("SELECT * FROM tbl_user ORDER BY id DESC"); 
-           $this->db->select('id,filename');  
+           $this->db->select('id,filename,title,description');  
 		   $this->db->where('iduser',$id);
            $this->db->from('file');  
            $query = $this->db->get();  
            return $query;  
-      }  
+      }
+
+		function delete_data($idfile){
+			$this->db->where("id", $idfile);
+			$this->db->delete("file");
+			
+		}
+			
  }
