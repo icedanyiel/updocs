cc998dc78961c7659d3dea95b48f5090c2a618d4
ArnoldNeuhold <neuhold.arnold95@gmail.com>
2017-05-30 21:38:55 +0300

    Update myaccount.php
    Now with description and delete button

--- application/views/myaccount.php
+++ application/views/myaccount.php
@@ -1,3 +1,4 @@
+
 <h1>MyAccount</h1>
    <h2>Welcome <?php echo $email; ?>!</h2>
     
@@ -5,7 +6,8 @@
            <table class="table table-bordered">  
                 <tr>  
                      <th>ID</th>  
-                     <th>File Name</th>     
+                     <th>File Name</th>
+					 <th>Description</th>
                 </tr> 
 			
 				
@@ -17,9 +19,12 @@
            ?>  
                 <tr>  
                      <td><?php echo $row->id; ?></td>  
-                     <td><?php echo "<a href='"."file:///C:/xampp/htdocs/updocs/public/".$row->filename."'>".$row->filename. "</a>"; ?></td> 
+                     <td><?php echo "<a href='"."file:///C:/xampp/htdocs/updocs/public/".$row->filename."'>".$row->title. "</a>"; ?></td>
+					 <td><?php echo $row->description; ?></td>
+					 <td><a href="MyAccount/delete_data" class="delete_data" id="<?php echo $row->id;?>">Delete</a></td>
                       
-                </tr>  
+                </tr> 
+				
 				<?php       
                 }  
            }  
@@ -30,7 +35,8 @@
                      <td colspan="2">No Data Found</td>  
                 </tr>  
            <?php  
-           }  
+           } 
+	 		   
            ?>  
 				
    <a href="MyAccount/logout" class="btn btn-default" >LogOut</a>
